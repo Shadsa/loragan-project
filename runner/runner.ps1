148a9467f279330ada80b34bf6308cd3e7bdcda9
Shadsa <zehava-bol@live.fr>
2019-03-11 15:49:59 +0100

    fix for script

--- runner/runner.ps1
+++ runner/runner.ps1
@@ -1,4 +1,4 @@
-param($scriptname = $mainpath, $version = "v1")
+param($scriptname = "", $version = "v1")
 
 $runnerpath = "./runtime/";
 $testpath = "../test_unit/";
@@ -6,8 +6,16 @@ $libpath = "../libraries/";
 $libname ="ThingSat_";
 $mainpath="../main.ino";
 
-Remove-Item $runnerpath"/*"
-Copy-Item -Path $testpath$scriptname -Destination $runnerpath;
+Remove-Item $runnerpath"/*";
+
+if(($scriptname -eq "")){
+    Copy-Item -Path $mainpath -Destination $runnerpath;
+}   
+else{
+    Copy-Item -Path $testpath$scriptname -Destination $runnerpath;
+}
+ 
+
 Copy-Item -Path $libpath$libname$version"/*" -Destination $runnerpath;
 
 
