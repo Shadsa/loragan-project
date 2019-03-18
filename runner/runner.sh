5827475cdb9c15403acaac101f6f068acff5d1d4
Shadsa <thibaud.vegreville@gmail.com>
2019-03-18 14:49:12 +0000

    runner.sh complete

--- runner/runner.sh
+++ runner/runner.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-if [ $# -ge 1 ]
+runnerpath="./runtime"
+testpath="../test_unit"
+libpath="../libraries"
+libname="ThingSat_"
+mainpath="../main.ino"
+
+if [ $# -eq 2 ]
 then
-    version=$1
+    version=$2
 else
     version="v1"
 fi
 
 echo "Current version :" $version
+rm -r $runnerpath/*
 
-runnerpath="./runtime/"
-testpath="../test_unit/"
-libpath="../libraries/"
-libname="ThingSat_"
+if [ $# -eq 1 ]
+then 
+    scriptname=$1
+    echo "Copy $scriptname"
+    cp $testpath/$scriptname $runnerpath/
+else 
+    echo Copy $mainpath
+    cp $mainpath $runnerpath/
+fi
 
-rm -r $runnerpath*
-cp -r "$testpath$scriptname" "$runnerpath"
-cp -r "$libpath$libname$version" "$runnerpath"
+cp $libpath/$libname$version/* $runnerpath/
