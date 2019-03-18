ad54cbe7136a1a2b71e721f5e9b37cfc55918e22
Shadsa <zehava-bol@live.fr>
2019-03-18 11:32:40 +0100

    First state for separate files

--- libraries/ThingSat_v1/factory.h
+++ libraries/ThingSat_v1/factory.h
@@ -2,35 +2,8 @@
 #define FACTORY_H
 
 #include <Arduino.h>
-#include <stdint.h>
-#include <Stream.h>
-#include <math.h>
-#include <stdio.h>
 #include "objects.h"
-
-#define xke		7.43669161E-2
-#define tothrd		6.6666666666666666E-1	/* 2/3 */
-#define ck2		5.413079E-4
-#define ae		1.0
-#define xkmper		6.378137E3		/* WGS 84 Earth radius km */
-#define SIMPLE_FLAG            0x000020
-#define twopi		6.28318530717958623	/* 2*Pi  */
-#define s		1.012229
-#define qoms2t		1.880279E-09
-#define xj3		-2.53881E-6		/* J3 Harmonic (WGS '72) */   
-#define ck4		6.209887E-7
-#define pio2		1.57079632679489656	/* Pi/2 */
-#define x3pio2		4.71238898038468967	/* 3*Pi/2 */
-#define pi		3.14159265358979323846	/* Pi */
-#define e6a		1.0E-6
-#define secday		8.6400E4	/* Seconds per day */
-#define omega_E		1.00273790934	/* Earth rotations/siderial day */
-#define f		3.35281066474748E-3	/* Flattening factor */
-#define SGP4_INITIALIZED_FLAG  0x000002
-#define epoch_start     2440587.50000
-#define minday		1.44E3			/* Minutes per day */
-#define deg2rad		1.745329251994330E-2	/* Degrees to radians */
-#define DEEP_SPACE_EPHEM_FLAG  0x000040
+#include "spg4.h"
 
 
 /* GPSPoint Manipulation*/
